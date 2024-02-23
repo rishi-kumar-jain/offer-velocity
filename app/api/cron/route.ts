1f17f5b2b688a98c4fa7c92202cf80e0c2fd2480
RJ <jainrishi932002@gmail.com>
2024-02-23 22:37:29 +0530

    hobby10s

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,7 +6,7 @@ import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } f
 import { EmailContent } from '@/types';
 import { NextResponse } from "next/server";
 
-export const maxDuration = 300;//5minutes
+export const maxDuration = 10;
 export const dynamic = 'force-dynamic'
 export const revalidate = 0;
 
